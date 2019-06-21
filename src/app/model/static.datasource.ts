77e95b216419c5773932a07eb85d9413cbc9c8f4
Lakshay Baweja <lakshay.baweja@pieinfosystems.com>
2019-06-21 13:43:25 +0530

    l

---
+++ src/app/model/static.datasource.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { Product } from "./product.model";
+import { Observable, from } from "rxjs";
+import { Order } from "./order.model";
+ 
+@Injectable()
+export class StaticDataSource {
+    private products: Product[] = [
+        new Product(1, "Product 1", "Category 1", "Product 1 (Category 1)", 100,"www"),
+        new Product(2, "Product 2", "Category 1", "Product 2 (Category 1)", 100, "www"),
+        new Product(3, "Product 3", "Category 1", "Product 3 (Category 1)", 100, "www"),
+        new Product(4, "Product 4", "Category 1", "Product 4 (Category 1)", 100, "www"),
+        new Product(5, "Product 5", "Category 1", "Product 5 (Category 1)", 100, "www"),
+        new Product(6, "Product 6", "Category 2", "Product 6 (Category 2)", 100, "www"),
+        new Product(7, "Product 7", "Category 2", "Product 7 (Category 2)", 100, "www"),
+        new Product(8, "Product 8", "Category 2", "Product 8 (Category 2)", 100, "www"),
+        new Product(9, "Product 9", "Category 2", "Product 9 (Category 2)", 100, "www"),
+        new Product(10, "Product 10", "Category 2", "Product 10 (Category 2)", 100, "www"),
+        new Product(11, "Product 11", "Category 3", "Product 11 (Category 3)", 100, "www"),
+        new Product(12, "Product 12", "Category 3", "Product 12 (Category 3)", 100, "www"),
+        new Product(13, "Product 13", "Category 3", "Product 13 (Category 3)", 100, "www"),
+        new Product(14, "Product 14", "Category 3", "Product 14 (Category 3)", 100, "www"),
+        new Product(15, "Product 15", "Category 3", "Product 15 (Category 3)", 100, "www"),
+    ];
+
+    getProducts(): Observable<Product[]> {
+        return from([this.products]);
+    }
+    saveOrder(order:Order): Observable<Order> {
+        console.log(JSON.stringify(order));
+        return from([order]);
+    }
+}
